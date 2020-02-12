867829685317bd9d85d4fb6ed4072c7dc0925077
Aleksander210 <aleksandar210@abv.bg>
2020-02-13 00:04:39 +0200

    added a new drive storing entry exams writen in java

---
+++ java PU/PlovdivUniversity_Entry_exams/src/exam2011_fmi_bachelor_pu_aleksandarKuzmov/Exam2011_main.java
@@ -0,0 +1,8 @@
+package exam2011_fmi_bachelor_pu_aleksandarKuzmov;
+
+public class Exam2011_main {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ java PU/PlovdivUniversity_Entry_exams/src/exam2013_fmi_plovdiv_aleksandar/Exam2013_main.java
@@ -0,0 +1,5 @@
+package exam2013_fmi_plovdiv_aleksandar;
+
+public class Exam2013_main {
+
+}
