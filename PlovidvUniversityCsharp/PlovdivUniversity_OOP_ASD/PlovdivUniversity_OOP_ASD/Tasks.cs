ff44c5b5da3a6d0aa90f5bcca6de278039a1844d
Aleksander210 <aleksandar210@abv.bg>
2020-05-23 16:52:49 +0300

    updated new added tak

--- PlovidvUniversityCsharp/PlovdivUniversity_OOP_ASD/PlovdivUniversity_OOP_ASD/Tasks.cs
+++ PlovidvUniversityCsharp/PlovdivUniversity_OOP_ASD/PlovdivUniversity_OOP_ASD/Tasks.cs
@@ -12,6 +12,7 @@ namespace PlovdivUniversity_OOP_ASD
         public void CarRace()
         {
             Dictionary<string, Dictionary<string, CurrentCar>> currentRacers = new Dictionary<string, Dictionary<string, CurrentCar>>();
+            Dictionary<string, int> currentPoints = new Dictionary<string, int>();
 
             string enterCarData = Console.ReadLine();
             string[] dataCollectedFromInut;
@@ -19,11 +20,27 @@ namespace PlovdivUniversity_OOP_ASD
             {
                 dataCollectedFromInut = enterCarData.Split(" ");
 
+                if (currentRacers.ContainsKey(dataCollectedFromInut[0]))
+                {
+                    if (!currentRacers[dataCollectedFromInut[0]].ContainsKey(dataCollectedFromInut[1]))
+                    {
+                        currentRacers[dataCollectedFromInut[0]].Add(dataCollectedFromInut[1], new CurrentCar(dataCollectedFromInut));
+                    }
+                   
+                }
+                else 
+                {
+                    currentRacers.Add(dataCollectedFromInut[0], new Dictionary<string, CurrentCar>());
+                    currentRacers[dataCollectedFromInut[0]].Add(dataCollectedFromInut[1], new CurrentCar(dataCollectedFromInut));
+                }
             }
+
+
         }
 
         private struct CurrentCar
         {
+            private string nameOfDriver;
             private string brand;
             private int horsePower;
             private double weight;
@@ -34,7 +51,8 @@ namespace PlovdivUniversity_OOP_ASD
 
             public CurrentCar(params string[] currentCarData) :this()
             {
-                
+
+                this.DriverName = string.Concat(currentCarData[0], currentCarData[1]);    
             this.Brand = currentCarData[2];
             this.Weight = double.Parse(currentCarData[3]);
             this.HorsePower = int.Parse(currentCarData[4]);
@@ -140,6 +158,20 @@ namespace PlovdivUniversity_OOP_ASD
                         break;
                 }
             }
+
+            public string DriverName
+            {
+                get
+                {
+                    return this.nameOfDriver;
+                }
+                private set
+                {
+                    this.nameOfDriver = value;
+                }
+                    
+            }
+
 
             public override string ToString()
             {
