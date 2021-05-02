f062fbc517d82f736d62f77ba434fce492c7a268
Aleksandar210 <aleksandar210@abv.bg>
2021-05-02 20:37:27 +0300

    updated new added task

--- StockMarketOOP_DB_APP/src/DBHelper.java
+++ StockMarketOOP_DB_APP/src/DBHelper.java
@@ -1,10 +1,14 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DBHelper {
 
-	private String connectionUrlSqlServer;
+	private String connectionUrlSqlServer = "jdbc:sqlserver://localhost\\sqlexpress;databaseName=StockMarketDB;integratedSecurity=true";;
 	
 	public void DBHelper() {
 		/*try {
@@ -14,8 +18,45 @@ public class DBHelper {
 			System.out.println(e.getMessage());
 		}
 		*/
+	}
+	
+	public void test() {
+		try {
+			Connection connection = DriverManager.getConnection(this.getConnectionString());
+			System.out.println("Connected succsfuly.");
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public HashMap<String,ArrayList<String>> getCountriesWithCitiesFromDataBase() {
+		HashMap<String,ArrayList<String>> countriesWithCities = null;
+		try {
+			Connection connection = DriverManager.getConnection(this.getConnectionString());
+			
+			Statement statementToGetCountries = connection.createStatement();
+			ResultSet currentCountriesWithCities = statementToGetCountries.executeQuery("SELECT CountryName, CityName FROM Countries\r\n"
+					+ "JOIN Cities ON Countries.ID = Cities.CountryID\r\n"
+					+ "ORDER BY CountryName ASC,CityName ASC");
+			
+			countriesWithCities = new HashMap<String,ArrayList<String>>();
+			
+			while(currentCountriesWithCities.next()) {
+				String countryName = currentCountriesWithCities.getString("CountryName");
+				String cityName = currentCountriesWithCities.getString("CityName");
+				if(!(countriesWithCities.containsKey(countryName))) {	
+					countriesWithCities.put(countryName, new ArrayList<String>());
+					countriesWithCities.get(countryName).add(cityName);
+				}else {
+					countriesWithCities.get(countryName).add(cityName);
+				}
+			}			
+			
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
 		
-		this.setConnectionString();
+		return countriesWithCities;
 	}
 	
 	public String getConnectionString() {
--- StockMarketOOP_DB_APP/src/Main.java
+++ StockMarketOOP_DB_APP/src/Main.java
@@ -1,12 +1,24 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {	
 public static void main(String[] args) {
 	DBHelper dbHelper = new DBHelper();
 	//RegistrationFrame registerFrame = new RegistrationFrame(dbHelper);
 	//LoginFrame login = new LoginFrame(dbHelper);
-	RegistrationFrame frame = new RegistrationFrame();
+	//RegistrationFrame frame = new RegistrationFrame();
+	
+	HashMap<String,ArrayList<String>> currentTestMap = dbHelper.getCountriesWithCitiesFromDataBase();
+	for(Map.Entry<String,ArrayList<String>> entry: currentTestMap.entrySet()) {
+		System.out.println(entry.getKey()+System.lineSeparator());
+		for(String towns:entry.getValue()) {
+			System.out.println(towns);
+		}
+		System.out.println(System.lineSeparator());
+	}
 }
 }
